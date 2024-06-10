fb70a9e55d1aa23c2e6db9b3d0f7337bb2f88ba6
farouqdaif1 <daiffarouq@gmail.com>
2024-06-10 20:21:19 +0300

    Fix language function

--- js/index.js
+++ js/index.js
@@ -18,23 +18,20 @@ async function fetchLanguageData(lang) {
 // Function to change language
 async function changeLanguage(lang) {
     await setLanguagePreference(lang);
-
     const langData = await fetchLanguageData(lang);
     updateContent(langData);
-    toggleArabicStylesheet(lang); // Toggle Arabic stylesheet
+    toggleEnglishStylesheet(lang); // Toggle English stylesheet
 }
 // Function to toggle Arabic stylesheet based on language selection
-function toggleArabicStylesheet(lang) {
+function toggleEnglishStylesheet(lang) {
     const head = document.querySelector('head');
     const link = document.querySelector('#styles-link');
-
-    if (link) {
+    if (link && lang === 'en') {
         head.removeChild(link); // Remove the old stylesheet link
-    } else if (lang === 'ar') {
         const newLink = document.createElement('link');
         newLink.id = 'styles-link';
         newLink.rel = 'stylesheet';
-        newLink.href = './assets/css/style-ar.css'; // Path to Arabic stylesheet
+        newLink.href = './css/style-en.css'; // Path to English stylesheet
         head.appendChild(newLink);
     }
 }
@@ -42,5 +39,5 @@ window.addEventListener('DOMContentLoaded', async () => {
     const userPreferredLanguage = localStorage.getItem('language') || 'ar';
     const langData = await fetchLanguageData(userPreferredLanguage);
     updateContent(langData);
-    toggleArabicStylesheet(userPreferredLanguage);
+    toggleEnglishStylesheet(userPreferredLanguage);
 });
