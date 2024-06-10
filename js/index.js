671033419149fbb5d7ede1a82b6c784f1103fb3e
farouqdaif1 <daiffarouq@gmail.com>
2024-06-10 19:29:01 +0300

    Add Switch language functionality

--- js/index.js
+++ js/index.js
@@ -23,3 +23,24 @@ async function changeLanguage(lang) {
     updateContent(langData);
     toggleArabicStylesheet(lang); // Toggle Arabic stylesheet
 }
+// Function to toggle Arabic stylesheet based on language selection
+function toggleArabicStylesheet(lang) {
+    const head = document.querySelector('head');
+    const link = document.querySelector('#styles-link');
+
+    if (link) {
+        head.removeChild(link); // Remove the old stylesheet link
+    } else if (lang === 'ar') {
+        const newLink = document.createElement('link');
+        newLink.id = 'styles-link';
+        newLink.rel = 'stylesheet';
+        newLink.href = './assets/css/style-ar.css'; // Path to Arabic stylesheet
+        head.appendChild(newLink);
+    }
+}
+window.addEventListener('DOMContentLoaded', async () => {
+    const userPreferredLanguage = localStorage.getItem('language') || 'ar';
+    const langData = await fetchLanguageData(userPreferredLanguage);
+    updateContent(langData);
+    toggleArabicStylesheet(userPreferredLanguage);
+});
